01f11dd23ba0df926c5e705780ee06ac7c6399dd
veeru153 <32760644+veeru153@users.noreply.github.com>
2020-11-12 22:51:58 +0530

    Updates

---
+++ precache-manifest.b85b7503c3b514a3dd83f277a6cf5cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0af02684244bc5dea9b0ba4394283f2",
+    "url": "/itc15-clothing-website/index.html"
+  },
+  {
+    "revision": "392b96ad9e92d7192f29",
+    "url": "/itc15-clothing-website/static/css/main.7484dbd2.chunk.css"
+  },
+  {
+    "revision": "206b40afac41d1600578",
+    "url": "/itc15-clothing-website/static/js/2.034df046.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/itc15-clothing-website/static/js/2.034df046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392b96ad9e92d7192f29",
+    "url": "/itc15-clothing-website/static/js/main.0d2636e0.chunk.js"
+  },
+  {
+    "revision": "0fb4addd0f432e884885",
+    "url": "/itc15-clothing-website/static/js/runtime-main.21ee699a.js"
+  }
+]);
